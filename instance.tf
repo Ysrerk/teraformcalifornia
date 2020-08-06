aa9183cf0d0a8182587d78599f2bd161e824e43f
Yasar ERKAN <ysr.erk2019@gmail.com>
2020-08-06 17:11:23 -0400

    it was added httpd

--- instance.tf
+++ instance.tf
@@ -7,12 +7,26 @@ resource "aws_instance" "terraexample" {
   ami           = var.ami_name 
   instance_type = "t2.micro"
   key_name = var.key_name
-  subnet_id = "aws_subnet.public_subnet.id"
+  subnet_id = aws_subnet.public_subnet.id
 
 
   tags={
     Name ="web_server"
   }
+  connection {
+    type = "ssh"
+    user = var.user_name
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+ }
+provisioner "remote-exec" {
+     inline = [
+        "sudo apt update",
+        "sudo apt install httpd -y",
+        "sudo systemctl start httpd",
+        "sudo systemctl enable httpd"
+   ]
+ }
  
 
 }
